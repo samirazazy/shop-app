9255cb154d84263b9e4808387b103faba40afffc
samirazazy <samiralazazy@gmail.com>
2020-01-04 01:06:54 +0200

    add auth

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -25,18 +25,18 @@ class Product with ChangeNotifier {
     notifyListeners();
   }
 
-  Future<void> toggleFavoriteStatus() async {
+  Future<void> toggleFavoriteStatus(String token, String userId) async {
     final oldStatus = isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
     final url =
-        'https://flutter-products-ee9e9.firebaseio.com/products/$id.json';
+        'https://flutter-products-ee9e9.firebaseio.com/userFavorites/$userId/$id.json?auth=$token';
     try {
-      final response = await http.patch(
+      final response = await http.put(
         url,
-        body: json.encode({
-          'isFavorite': isFavorite,
-        }),
+        body: json.encode(
+          isFavorite,
+        ),
       );
       if (response.statusCode >= 400) {
         _setFavValue(oldStatus);
